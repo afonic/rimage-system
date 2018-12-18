1eaf0e2319d9f9612699c9a27b4f607b49ad5d27
Iosif Chatzimichail <afonic@gmail.com>
2018-12-18 21:24:37 +0200

    Add regenerate button (commit 1)

--- rimage.php
+++ rimage.php
@@ -6,6 +6,11 @@ jimport( 'joomla.plugin.plugin' );
 
 class plgSystemRImage extends JPlugin
 {
+    function __construct(&$subject, $params)
+    {
+        parent::__construct($subject, $params);
+    }
+
     function onAfterInitialise() {
     	// Include the autoloader for the plugin
         require_once(JPATH_ROOT.'/plugins/k2/rimage/vendor/autoload.php');
@@ -14,15 +19,38 @@ class plgSystemRImage extends JPlugin
         $app = JFactory::getApplication();
 
         if ($app->isAdmin() and ($app->input->get('rimage') == 'regen')) {
+            $regenerator = new Reach\rImageForceRegeneration;
+            $regenerator->regenerate();
+        }        
 
-	        $regenerator = new Reach\rImageForceRegeneration;
-	        $regenerator->regenerate();
-
+        if ($app->isAdmin() and ($app->input->get('rimage') == 'item')) {
+            $id = $app->input->get('rid');
+            $catid = $app->input->get('rcatid');
+            $gallery = $app->input->get('rgallery');
+            if ($id && $catid) {
+                $regenerator = new Reach\rImageForceRegeneration;
+                $regenerator->regenerateSingle($id, $catid, $gallery);
+            }
         }
     }
-    
-    function onAfterRoute() {
-        JHtml::_('jquery.framework');
+
+    function onRenderAdminForm(&$item, $type, $tab = '') {
+        $app = JFactory::getApplication();
+        if (($item->id) && ($type == 'item') && ($tab == 'content')) {
+            $this->render($item);
+        }
+    }
+
+    function render($item) {
+        $options = array();
+        $options[] = 'data-id="'.$item->id.'"';
+        $options[] = 'data-category="'.$item->catid.'"';
+        if ($item->gallery) {
+            $options[] = 'data-gallery="yes"';
+        }
+        echo '<div id="rimage-options" '.implode(' ', $options).' style="display: none"></div>';
+        $doc = JFactory::getDocument();
+        $doc->addScript('/plugins/system/rimage/assets/script.js');
     }
 
 }
