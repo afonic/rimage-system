527ae78aa63c4de8d5cd1d2b4810c45a9f79a9e1
Iosif Chatzimichail <afonic@gmail.com>
2018-12-19 18:14:38 +0200

    Added the ability to change gallery's order

---
+++ assets/regen.js
@@ -0,0 +1,29 @@
+jQuery(document).ready(function($) {
+	if ($('#rimage-options').length > 0) {
+		var html = `
+		<div class="btn-wrapper" id="toolbar-rimage-regen">
+			<button id="rimage-regen" class="btn btn-small button-cancel">
+				<span class="icon-refresh" aria-hidden="true"></span>
+				Renegerate images
+			</button>
+		</div>`;
+		$("#toolbar").append(html);
+
+		$('#rimage-regen').click(function() {
+			$(this).attr("disabled", true);
+			var r = $('#rimage-options');
+			jQuery.ajax({
+				method: "POST",
+				url: '/administrator/index.php',
+				data: { rimage: 'item', rid: r.data('id'), rcatid: r.data('category'), rgallery: r.data('gallery') }
+			})
+			.done(function() {
+				$('#toolbar').notify("Images have been sucessfully regenerated.", {className: "success", elementPosition: "bottom center"});
+				$('#rimage-regen').attr("disabled", false);
+			})
+			.error(function() {
+				$('#toolbar').notify("Images have NOT been sucessfully regenerated. Please contact admin.", {className: "error", elementPosition: "bottom center"});
+			});
+		})
+	}
+});
--- rimage.php
+++ rimage.php
@@ -6,10 +6,6 @@ jimport( 'joomla.plugin.plugin' );
 
 class plgSystemRImage extends JPlugin
 {
-    // function __construct(&$subject, $params)
-    // {
-    //     parent::__construct($subject, $params);
-    // }
 
     function onAfterInitialise() {
     	// Include the autoloader for the plugin
@@ -31,18 +27,37 @@ class plgSystemRImage extends JPlugin
                 $regenerator = new Reach\rImageForceRegeneration;
                 $regenerator->regenerateSingle($id, $catid, $gallery);
             }
-        }
-    }
+        }        
 
-    function onRenderAdminForm(&$item, $type, $tab = '') {
-        if ($this->params['showregen'] != '1') {
-            if (($item->id) && ($type == 'item') && ($tab == 'content')) {
-                $this->render($item);
+        if ($app->isAdmin() and ($app->input->get('rimage') == 'order')) {
+            $id = $app->input->get('rid');
+            $order = $app->input->get('rdata', null, PATH);
+            if ($id && $order) {
+                $this->saveOrderJson($id, $order);
             }
         }
     }
 
-    function render($item) {
+    function onRenderAdminForm(&$item, $type, $tab = '') {
+        if (($item->id) && ($type == 'item') && ($tab == 'content')) {
+            if (($this->params['showregen'] != '1') || ($this->params['showorder'] != '1')) {
+                $this->renderNotify();
+            }
+            if ($this->params['showregen'] != '1') {            
+                $this->renderRegenerate($item);            
+            }
+            if ($item->gallery && ($this->params['showorder'] != '1')) {
+                $this->renderOrder($item);
+            }            
+        }
+    }
+
+    function renderNotify() {
+        $doc = JFactory::getDocument();
+        $doc->addScript('/plugins/system/rimage/assets/notify.js');
+    }
+
+    function renderRegenerate($item) {
         $options = array();
         $options[] = 'data-id="'.$item->id.'"';
         $options[] = 'data-category="'.$item->catid.'"';
@@ -51,7 +66,66 @@ class plgSystemRImage extends JPlugin
         }
         echo '<div id="rimage-options" '.implode(' ', $options).' style="display: none"></div>';
         $doc = JFactory::getDocument();
-        $doc->addScript('/plugins/system/rimage/assets/script.js');
+        $doc->addScript('/plugins/system/rimage/assets/regen.js');
+    }
+
+    function renderOrder($item) {
+        $files = $this->getImages($item->id);
+        $images = '';
+        foreach ($files as $file) {
+            $path = str_replace(JPATH_ROOT, '', $file->path);
+            $images .= '<div class="rthumb" data-id="'.$path.'"><img src="'.$path.'" /></div>'.PHP_EOL;
+        }
+        $modal = '
+        <div id="rimage-order" data-rid="'.$item->id.'" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="Order Gallery">
+          <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header" style="padding: 16px">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title">Order gallery items</h4>
+                    <span>Drag to reorder, order is saved automatically.</span>
+                </div>
+                <div class="modal-body" style="width: calc(100% - 1rem); padding: 0.5rem">
+                    <div id="rthumbs" class="rthumbs-container">'.$images.'</div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+          </div>
+        </div>
+        ';
+        echo $modal;
+        $doc = JFactory::getDocument();
+        $doc->addScript('/plugins/system/rimage/assets/sortable.js');
+        $doc->addScript('/plugins/system/rimage/assets/order.js');
+        $doc->addStylesheet('/plugins/system/rimage/assets/order.css');
+    }
+
+    function getImages($id) {
+        $files = new Reach\rImageFiles($id);
+        return $files->getFiles();
+    }    
+
+    function getDir($id) {
+        $files = new Reach\rImageFiles($id);
+        return $files->getDir();
+    }
+
+    function saveOrderJson($id, $order) {
+        $path = $this->getDir($id).'order.json';
+        $json_data = json_encode($order);
+
+        if (file_exists($path)) {
+            unlink($path);
+        }
+
+        if (!file_put_contents($path, $json_data)) {
+            JFactory::getApplication()->enqueueMessage("Couldn't write JSON file! Are you sure the path is correct?", 'error');
+        }
+        else {
+            JFactory::getApplication()->enqueueMessage("JSON generation OK.", 'message');
+        }
     }
 
 }
